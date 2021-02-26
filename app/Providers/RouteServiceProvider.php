7d1f227fbb9eeb5fe25b3b878179199c8dd6cbb3
Jay <jayfructuoso@gmail.com>
2021-02-26 18:51:29 +0800

    disabled auth api, temporarily

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api/v1')
-                ->middleware('auth:api')
+                ->middleware('web')
+                // ->middleware('auth:api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api/v1/api.php'));
 
