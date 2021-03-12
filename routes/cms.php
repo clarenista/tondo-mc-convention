7d28fb1256c3f8b1cb64e246d3de17d0f8bb56ef
Jay <jayfructuoso@gmail.com>
2021-03-12 13:41:09 +0800

    update

--- routes/cms.php
+++ routes/cms.php
@@ -7,22 +7,31 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [UserController::class, 'loginView'])->name('login');
-Route::get('/logout', [UserController::class, 'logoutUser']);
-Route::post('/login', [UserController::class, 'loginUser']);
 
-Route::group(['middleware' => ['role:admin']], function () {
-    Route::get('/register', [UserController::class, 'register']);
-    Route::post('/register', [UserController::class, 'storeRegistration']);
-    Route::get('/users', [UserController::class, 'index']);
+// Route::group(['middleware' => ['role:admin']], function () {
 
-    Route::resource('booths', BoothController::class);
-});
+// });
 
-Route::get('/', [HomeController::class, 'index'])->middleware('auth');
 
-Route::group(['as' => 'cms.', 'middleware' => ['role:sponsor']], function () {
-    Route::resource('assets', AssetController::class);
-    Route::resource('banners', BannerController::class);
+// Route::group(['as' => 'cms.', 'middleware' => ['role:sponsor']], function () {
 
+// });
+
+Route::name('cms.')->group(function () {
+
+    Route::get('/login', [UserController::class, 'loginView'])->name('login');
+    Route::get('/logout', [UserController::class, 'logoutUser']);
+    Route::post('/login', [UserController::class, 'loginUser']);
+
+    Route::get('/', [HomeController::class, 'index'])->middleware('auth');
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::resource('users', UserController::class);
+        Route::resource('guests', UserController::class);
+        Route::resource('booths', BoothController::class);
+    });
+    Route::middleware(['role:sponsor'])->group(function () {
+        Route::resource('assets', AssetController::class);
+        Route::resource('banners', BannerController::class);
+    });
 });
