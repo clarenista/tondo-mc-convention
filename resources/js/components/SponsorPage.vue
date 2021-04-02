b48690d1958bfaed57b86655d687edfc00138cd9
Jay <jayfructuoso@gmail.com>
2021-04-02 22:45:44 +0800

    fix booth unauth

--- resources/js/components/SponsorPage.vue
+++ resources/js/components/SponsorPage.vue
@@ -6,7 +6,7 @@
            <Modal :value="value" v-if="selectedHotspot != null">
                 <template v-slot:title >
                     <h1>{{selectedHotspot.name}}</h1>
-                </template>           
+                </template>
                 <template v-slot:body >
                     <div class="row">
                         <div class="col-12">
@@ -15,7 +15,7 @@
 
                         <!-- CONTACT US FORM -->
                         <template v-if="selectedHotspot.name == 'contact-us'">
-                        
+
                         <div class="col-12 mb-3">
                           <!-- {{selectedHotspot.assets[0].url}} -->
                           <div class="input-group">
@@ -25,7 +25,7 @@
                               <input type="text" class="form-control" v-model="subject" placeholder="Subject" aria-label="">
                           </div>
                         </div>
-                        
+
                         <div class="col-12 mb-3">
                           <div class="input-group">
                               <span class="input-group-prepend">
@@ -87,10 +87,10 @@
                         <!-- CONTACT US FORM -->
 
                     </div>
-                </template>  
+                </template>
                 <template v-slot:footer >
                     <button class="btn btn-secondary" type="button" @click="handleCloseModal">Close</button>
-                </template>          
+                </template>
            </Modal>
         <div class="booth-container">
             <img class="centered" src="/images/lt.png">
@@ -101,9 +101,9 @@
             <a v-if="booth_details.hotspots != null" href="" class="hotspot" @click.prevent="handleSelectHotspot(item)" v-for="(item, index) in booth_details.hotspots" :key="index" :style="addStyle(item)">
                 <span class="hotspot--cta"></span>
             </a>
-            
+
         </section>
-        
+
     </div>
 </template>
 <script>
@@ -122,7 +122,7 @@ export default {
     //  fetch("/images/lt.mp4", {method:"HEAD"})
     // .then(response => response.headers.get("Content-Type"))
     // .then(type => console.log(`.${type.replace(/.+\/|;.+/g, "")}`));
-        
+
     },
     data() {
         return {
@@ -145,7 +145,7 @@ export default {
     methods:{
         async init(){
             const wrapper = document.querySelector('.hotspots--wrapper');
-            let {data} = await axios.get('/api/v1/booths/'+this.id);
+            let {data} = await axios.get('/api/v1/booths/'+this.id+'?api_token='+localStorage.getItem('access_token'));
             this.booth_details = data
             // $(window)
             //     .resize(function() {
@@ -156,7 +156,7 @@ export default {
         rescale(){
 
             // let parent, ratio, uiratios, dimension, size;
-            // parent = this.$refs.parent 
+            // parent = this.$refs.parent
             // ratio = this.$refs.uiratio.dataset.uiRatio
             // uiratios = ratio.split(',')
             // dimension = {
@@ -165,10 +165,10 @@ export default {
             // }
             // size = Math.min(parent.clientWidth / dimension.w, parent.clientHeight / dimension.h);
             // this.style = {width: dimension.w * size+"px", height: dimension.h * size+"px", }
-                      
+
         },
-        
-        
+
+
         addStyle(item){
             return {right: item.y+'%', top: item.x+'%'}
         },
@@ -200,7 +200,7 @@ export default {
           if(data.status === 'ok'){
             this.successMessage = true
           }
-          
+
         }
     }
 
@@ -258,7 +258,7 @@ body div {
 
 .hotspots--figure {
   max-width: 100%;
-  
+
 }
 
 .hotspot {
@@ -368,7 +368,7 @@ body div {
   .hotspot--title {
     display: none;
   }
-  
+
   .hotspot--cta {
     width: 20px;
     height: 20px;
