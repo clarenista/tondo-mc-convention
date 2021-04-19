500d7f57345ed6bb5167a4f78af4e1456f2450f8
jay <jay@local>
2021-04-20 06:48:09 +0800

    update

--- resources/js/components/MeetingHall/MeetingHall.js
+++ resources/js/components/MeetingHall/MeetingHall.js
@@ -5,23 +5,37 @@ export default {
         zoomBtn.style.display = "none";
         this.getZoomType(vue).then((result) => {
             let program = result.data;
-            if (program.enabled == 1 || (program.type == 'all' && userType == 'Diplomate')) {
+            if (
+                program.enabled == 1 &&
+                (program.type == "all" ||
+                    (program.type == "private" && userType == "Diplomate"))
+            ) {
                 zoomBtn.style.display = "";
                 zoomBtn.addEventListener("click", () => {
-                    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                    if (
+                        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                            navigator.userAgent
+                        )
+                    ) {
                         this.openZoomMobile(vue);
                     } else {
-                        window.location = '/meeting-hall';
+                        window.location = "/meeting-hall";
                     }
                 });
             }
         });
     },
     async openZoomMobile(vue) {
-        let { data } = await vue.axios.get(`/api/v1/guests/zoom/join/mobile?api_token=${localStorage.getItem('access_token')}`);
+        let { data } = await vue.axios.get(
+            `/api/v1/guests/zoom/join/mobile?api_token=${localStorage.getItem(
+                "access_token"
+            )}`
+        );
         window.open(data, "_blank");
     },
     getZoomType(vue) {
-        return vue.axios.get(`/api/v1/program?api_token=${localStorage.getItem('access_token')}`);
-    }
-}
+        return vue.axios.get(
+            `/api/v1/program?api_token=${localStorage.getItem("access_token")}`
+        );
+    },
+};
