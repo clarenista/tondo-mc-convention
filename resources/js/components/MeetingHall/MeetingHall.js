eb02e05c3a2b5314fd2a3a4358d6c788774da93d
Jay <jayfructuoso@gmail.com>
2021-04-20 11:09:00 +0800

    update meeting hall

--- resources/js/components/MeetingHall/MeetingHall.js
+++ resources/js/components/MeetingHall/MeetingHall.js
@@ -1,42 +1,43 @@
 export default {
     init(vue) {
-        let userType = vue.$store.getters.user.classification;
         let zoomBtn = vue.$el.querySelector(".open-zoom-meeting");
         zoomBtn.style.display = "none";
-        this.getZoomType(vue).then((result) => {
-            let program = result.data;
-            console.log(program.enabled, program.type, userType);
-            if (
-                program.enabled == 1 &&
-                (program.type == "all" ||
-                    (program.type == "private" && (userType == "Diplomate" || userType == "Fellow")))
-            ) {
+        this.isAllowed(vue).then((result) => {
+            if (result) {
                 zoomBtn.style.display = "";
                 zoomBtn.addEventListener("click", () => {
-                    if (
-                        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-                            navigator.userAgent
-                        )
-                    ) {
-                        this.openZoomMobile(vue);
-                    } else {
-                        window.location = "/meeting-hall";
-                    }
+                    this.isAllowed(vue).then((result) => {
+                        if (result) {
+                            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                                this.openZoomMobile(vue);
+                            } else {
+                                window.location = "/meeting-hall";
+                            }
+                        } else {
+                            zoomBtn.style.display = "none";
+                        }
+                    });
                 });
             }
-        });
+        })
     },
     async openZoomMobile(vue) {
-        let { data } = await vue.axios.get(
-            `/api/v1/guests/zoom/join/mobile?api_token=${localStorage.getItem(
-                "access_token"
-            )}`
-        );
+        let { data } = await vue.axios.get(`/api/v1/guests/zoom/join/mobile?api_token=${localStorage.getItem("access_token")}`);
         window.open(data, "_blank");
     },
+    async isAllowed(vue) {
+        let { data } = await vue.axios.get(`/api/v1/program?api_token=${localStorage.getItem("access_token")}`);
+        let program = data;
+        console.log(program);
+        if (program.enabled == 1) {
+            if (program.type == "all")
+                return true;
+            if (program.type == "private" && (userType == "Diplomate" || userType == "Fellow"))
+                return true;
+        }
+        return false;
+    },
     getZoomType(vue) {
-        return vue.axios.get(
-            `/api/v1/program?api_token=${localStorage.getItem("access_token")}`
-        );
+        return vue.axios.get(`/api/v1/program?api_token=${localStorage.getItem("access_token")}`);
     },
 };
