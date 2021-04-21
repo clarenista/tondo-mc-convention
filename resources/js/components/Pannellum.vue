dc82699b4c7517352e220facaaa48323c1eeaa8e
mj-git <contact.mjpantaleon@gmail.com>
2021-04-21 21:48:29 +0800

    Remove crosshair

--- resources/js/components/Pannellum.vue
+++ resources/js/components/Pannellum.vue
@@ -201,7 +201,7 @@ export default {
                 "autoLoad": true,
                 "showControls": false,
                 // uncomment the code below to get the PITCH and YAW of hotspot - console
-                "hotSpotDebug": true,
+                // "hotSpotDebug": true,
             },
 
             "scenes": {
