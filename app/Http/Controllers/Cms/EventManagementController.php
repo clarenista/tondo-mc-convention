0e5a4215ce1d1c07df9d3a10995df7bb5bd667ac
jay <jay@local>
2021-04-26 12:28:13 +0800

    db dump

--- app/Http/Controllers/Cms/EventManagementController.php
+++ app/Http/Controllers/Cms/EventManagementController.php
@@ -5,16 +5,32 @@ namespace App\Http\Controllers\Cms;
 use App\Http\Controllers\Controller;
 use App\Models\Event;
 use App\Models\Program;
-use Illuminate\Http\Request;
 
 class EventManagementController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
 
         $event = Event::first();
         $program = Program::first();
 
         return view('cms.event-management.index', compact('event', 'program'));
+    }
+
+    public function downloadDb()
+    {
+
+        $host = config('database.connections.mysql.host');
+        $username = config('database.connections.mysql.username');
+        $password = config('database.connections.mysql.password');
+        $database = config('database.connections.mysql.database');
+        $ts = time();
+        $file = date('Y-m-d-His', $ts) . '-dump-' . $database . '.sql';
+        $file = \storage_path($file);
+        $command = sprintf('mysqldump -h %s -u %s -p\'%s\' %s > %s', $host, $username, $password, $database, $file);
+        exec($command);
+
+        return response()->download(\storage_path($file));
     }
 }
