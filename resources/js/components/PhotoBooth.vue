9f585c378231b4ca107bcae02c1a4648a972a989
Lester Lou Reyes <clarenista@gmail.com>
2021-10-24 22:55:01 +0800

    update

--- resources/js/components/PhotoBooth.vue
+++ resources/js/components/PhotoBooth.vue
@@ -1,46 +1,46 @@
 <template lang="">
     <div class="row d-flex justify-content-center">
-        
-        <div id="app" class="web-camera-container">
-            <div class="camera-button">
-                <button type="button" class="button is-rounded" :class="{ 'is-primary' : !isCameraOpen, 'is-danger' : isCameraOpen}" @click="toggleCamera">
-                    <span v-if="!isCameraOpen">Open Camera</span>
-                    <span v-else>Close Camera</span>
-                </button>
-            </div>
-            
-            <div v-show="isCameraOpen && isLoading" class="camera-loading">
-                <ul class="loader-circle">
-                <li></li>
-                <li></li>
-                <li></li>
-                </ul>
-            </div>
-            
-            <div v-if="isCameraOpen" v-show="!isLoading" class="camera-box rounded" :class="{ 'flash' : isShotPhoto }">
-                
-                <div class="camera-shutter" :class="{'flash' : isShotPhoto}"></div>
-                <div class="capture" ref="printMe" v-if="page == 1" >
-                    <video  ref="camera" :width="450" :height="337.5" autoplay style="padding:30px; "></video>
-                    <img class="result-image" style="opacity: 1;" src="https://vbth.2021lxatheultimatedestiny.com/customers/hRQsR12/thRQ-QQhU/assets/frames/6377051768590074810.png?v=425">
-                </div>
-                
-                <img class="img-fluid" :src="output" alt="" v-else>
-                
+        <div class="card">
+
+          <div class="card-body">
+            <h5 class="card-title">PhotoBooth</h5>
+            <button type="button" class="button is-rounded" :class="{ 'is-primary' : !isCameraOpen, 'is-danger' : isCameraOpen}" @click="toggleCamera">
+              <span v-if="!isCameraOpen">Open Camera</span>
+              <span v-else>Close Camera</span>
+            </button>
+            <div v-if="isCameraOpen" v-show="!isLoading" class="camera-box" :class="{ 'flash' : isShotPhoto }">
+          
+              <div class="camera-shutter" :class="{'flash' : isShotPhoto}"></div>
+
+              <div v-show="!isPhotoTaken">
+                <img style="opacity: 1;" class="position-absolute"  :width="450" :height="337.5" src="https://cdn.pixabay.com/photo/2017/11/10/16/59/frame-2936877_960_720.png">
+                <video  ref="camera" :width="450" :height="337.5" autoplay></video>
+              </div>  
+              
+              <div ref="printMe" v-show="isPhotoTaken">
+                <img  style="opacity: 1;" :width="450" :height="337.5" class="position-absolute" src="https://cdn.pixabay.com/photo/2017/11/10/16/59/frame-2936877_960_720.png">
+                <canvas  id="photoTaken" ref="canvas" :width="450" :height="337.5"></canvas>
+                <!-- OUTPUT -->
+              </div>
             </div>
             
             <div v-if="isCameraOpen && !isLoading" class="camera-shoot">
-                <button type="button" class="button" @click="takePhoto">
+              <button type="button" class="button" @click="takePhoto">
                 <img src="https://img.icons8.com/material-outlined/50/000000/camera--v2.png">
-                </button>
+              </button>
             </div>
             
             <div v-if="isPhotoTaken && isCameraOpen" class="camera-download">
-                <a id="downloadPhoto" download="my-photo.jpg" class="button" role="button" @click="downloadImage">
+              <a id="downloadPhoto" download="my-photo.jpg" class="button" role="button" @click="downloadImage">
                 Download
-                </a>
+              </a>
             </div>
+          </div>
+          <div class="card-footer">
+            Footer
+          </div>
         </div>
+
     </div>
 
 </template>
@@ -48,17 +48,16 @@
 import html2canvas from 'html2canvas'
 export default {
     mounted() {
-        this.createCameraElement()
+      this.createCameraElement();
     },
-data() {
+    data() {
     return {
       isCameraOpen: true,
       isPhotoTaken: false,
       isShotPhoto: false,
       isLoading: false,
       link: '#',
-      output: null,
-      page: 1
+      output: null
     }
   },
   
@@ -105,15 +104,17 @@ data() {
     },
     
     async takePhoto() {
-      
-        const el = this.$refs.printMe;
-        // add option type to get the image version
-        // if not provided the promise will return 
-        // the canvas.
-        const options = {
-            type: 'dataURL'
-        }
-        
+      const el = this.$refs.printMe;
+      // add option type to get the image version
+      // if not provided the promise will return 
+      // the canvas.
+      const options = {
+        type: 'dataURL',
+        logging: true,
+        letterRendering: 1,
+        allowTaint: false,
+        useCORS: true,
+      }
       if(!this.isPhotoTaken) {
         this.isShotPhoto = true;
 
@@ -126,133 +127,29 @@ data() {
       
       this.isPhotoTaken = !this.isPhotoTaken;
       
-      // const context = this.$refs.canvas.getContext('2d');
-      
-      // console.log(context)
-      // context.drawImage(this.$refs.camera, 0, 0, 450, 337.5);
-      this.output = await this.$html2canvas(el, options);
-      this.page = 2
+      const context = this.$refs.canvas.getContext('2d');
+      context.drawImage(this.$refs.camera, 0, 0, 450, 337.5);
+      this.output = await this.$html2canvas(el,options);      
     },
     
     downloadImage() {
       const download = document.getElementById("downloadPhoto");
-      const canvas = document.getElementById("photoTaken").toDataURL("image/jpeg")
-    .replace("image/jpeg", "image/octet-stream");
-      download.setAttribute("href", canvas);
-    }
-  }    
+      
+      download.setAttribute("href", this.output);
+    },
+
+  }
 }
 </script>
 <style scoped>
 .result-image{
-  position: absolute;
-  top: 0;
-  width: 300px;
-  height: 300px;
-  left: 0;
-}
-body {
-  display: flex;
-  justify-content: center;
+position: absolute;
+    top: 120px;
+    width: 550px;
+    height: 350px;
+    left: 450px;
 }
 
-.web-camera-container {
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 500px;
-  background-color: #ccc;
-}
-.web-camera-container .camera-button {
-  margin-bottom: 2rem;
-}
-.web-camera-container .camera-box .camera-shutter {
-  opacity: 0;
-  width: 450px;
-  height: 337.5px;
-  background-color: #fff;
-  position: absolute;
-}
-.web-camera-container .camera-box .camera-shutter.flash {
-  opacity: 1;
-}
-.web-camera-container .camera-shoot {
-  margin: 1rem 0;
-}
-.web-camera-container .camera-shoot button {
-  height: 60px;
-  width: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 100%;
-}
-.web-camera-container .camera-shoot button img {
-  height: 35px;
-  object-fit: cover;
-}
-.web-camera-container .camera-loading {
-  overflow: hidden;
-  height: 100%;
-  position: absolute;
-  width: 100%;
-  min-height: 150px;
-  margin: 3rem 0 0 -1.2rem;
-}
-.web-camera-container .camera-loading ul {
-  height: 100%;
-  position: absolute;
-  width: 100%;
-  z-index: 999999;
-  margin: 0;
-}
-.web-camera-container .camera-loading .loader-circle {
-  display: block;
-  height: 14px;
-  margin: 0 auto;
-  top: 50%;
-  left: 100%;
-  transform: translateY(-50%);
-  transform: translateX(-50%);
-  position: absolute;
-  width: 100%;
-  padding: 0;
-}
-.web-camera-container .camera-loading .loader-circle li {
-  display: block;
-  float: left;
-  width: 10px;
-  height: 10px;
-  line-height: 10px;
-  padding: 0;
-  position: relative;
-  margin: 0 0 0 4px;
-  background: #999;
-  animation: preload 1s infinite;
-  top: -50%;
-  border-radius: 100%;
-}
-.web-camera-container .camera-loading .loader-circle li:nth-child(2) {
-  animation-delay: 0.2s;
-}
-.web-camera-container .camera-loading .loader-circle li:nth-child(3) {
-  animation-delay: 0.4s;
-}
-@keyframes preload {
-  0% {
-    opacity: 1;
-  }
-  50% {
-    opacity: 0.4;
-  }
-  100% {
-    opacity: 1;
-  }
-}
+
+
 </style>
