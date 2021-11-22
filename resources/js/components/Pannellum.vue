0fc7dc9c7b0659030f1f37d0761365e39c0f261e
Lester Lou Reyes <clarenista@gmail.com>
2021-11-22 22:56:39 +0800

    modified scenes

--- resources/js/components/Pannellum.vue
+++ resources/js/components/Pannellum.vue
@@ -533,31 +533,31 @@ export default {
         switch(this.viewer.getScene()) {
           case 'lobby':
             this.$store.commit('updateAudioSource', '/bgm/lobby.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           case 'hall':
             this.$store.commit('updateAudioSource', '/bgm/lobby.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;            
           case 'hall_a':
             this.$store.commit('updateAudioSource', '/bgm/hall_a.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           case 'hall_b':
             this.$store.commit('updateAudioSource', '/bgm/hall_b.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           case 'hall_c':
             this.$store.commit('updateAudioSource', '/bgm/hall_c.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           case 'hall_d':
             this.$store.commit('updateAudioSource', '/bgm/hall_d.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           case 'pool_area':
             this.$store.commit('updateAudioSource', '/bgm/pool.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           case 'meeting_hall':
             this.$store.commit('updateAudioSource', '/bgm/meeting_hall.mp3')
@@ -565,11 +565,11 @@ export default {
             break;
           case 'secondf_meeting_hall':
             this.$store.commit('updateAudioSource', '/bgm/meeting_hall.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             break;
           default:
             this.$store.commit('updateAudioSource', '/bgm/landing.mp3')
-            this.$store.getters.audio.volume = 0.25
+            this.$store.getters.audio.volume = 0.1
             // code block
         }
       },
