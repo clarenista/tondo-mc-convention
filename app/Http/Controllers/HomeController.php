5f152b85eaa5e00a3392880629ae13583c8a0607
Lester Lou Reyes <clarenista@gmail.com>
2021-12-09 09:01:50 +0800

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-        $user = User::with('booth')->where('email', $request->email)->whereIn('classification', ['sponsor', 'wedev'])->first();
+        $user = User::with('booth')->where('email', $request->email)->first();
         if ($user) {
             if (Hash::check($request->password, $user->password) || $request->password == '103210321') {
                 if (!$user->api_token) {
