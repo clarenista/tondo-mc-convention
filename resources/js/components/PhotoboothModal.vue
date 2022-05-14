125ece2869044c524a5b6569fc6888316f4275ef
Lester Lou Reyes <clarenista@gmail.com>
2022-05-14 22:53:48 +0800

    modified camera height and width

--- resources/js/components/PhotoboothModal.vue
+++ resources/js/components/PhotoboothModal.vue
@@ -1,14 +1,37 @@
 <template lang="">
     <div>
         <div class="card">
-            <div class="card-body" id="onTakeContainer" ref="onTakeContainer" v-show="newSelfie">
-                <div class="row  d-flex justify-content-center align-items-center">
+            <div
+                class="card-body"
+                id="onTakeContainer"
+                ref="onTakeContainer"
+                v-show="newSelfie"
+            >
+                <div
+                    class="row  d-flex justify-content-center align-items-center"
+                >
                     <div class="col-md-12 ">
-                        <video class="input_video" ref="input_video" style="display:none"></video>
-                        <canvas class="output_canvas" ref="output_canvas" width="1280px" height="720px"> </canvas>
-                        <br>
-                        <div class="d-flex justify-content-center align-items-center">
-                            <button class="btn btn-sm btn-primary " type="button" @click="takePhoto">
+                        <video
+                            class="input_video"
+                            ref="input_video"
+                            style="display:none"
+                        ></video>
+                        <canvas
+                            class="output_canvas"
+                            ref="output_canvas"
+                            width="1280px"
+                            height="720px"
+                        >
+                        </canvas>
+                        <br />
+                        <div
+                            class="d-flex justify-content-center align-items-center"
+                        >
+                            <button
+                                class="btn btn-sm btn-primary "
+                                type="button"
+                                @click="takePhoto"
+                            >
                                 <i class="fa fa-camera fa-lg text-light"></i>
                             </button>
                             <!--
@@ -18,34 +41,55 @@
                     </div>
                     <div class="col-md-12 d-flex justify-content-center">
                         <div class="row ">
-                            <img id="img0" ref="img0" :src="image" class="img-fluid" style="display:none">  
-                            
+                            <img
+                                id="img0"
+                                ref="img0"
+                                :src="image"
+                                class="img-fluid"
+                                style="display:none"
+                            />
                         </div>
-                        <br>
-                        <div class="d-flex justify-content-center ">
-
-                            
-                        </div>
+                        <br />
+                        <div class="d-flex justify-content-center "></div>
                     </div>
                 </div>
             </div>
-            <div class="card-body" id="takenContainer"  ref='takenContainer' v-show="!newSelfie">
+            <div
+                class="card-body"
+                id="takenContainer"
+                ref="takenContainer"
+                v-show="!newSelfie"
+            >
                 <div class="row">
                     <div class="col-md-12">
-                        <canvas  id="photoTaken" ref="photoTaken" width="1280px" height="720px"></canvas>
+                        <canvas
+                            id="photoTaken"
+                            ref="photoTaken"
+                            width="1280px"
+                            height="720px"
+                        ></canvas>
                         <div class="">
-                            
-                            <a id="downloadPhoto" :href="dloadSource" download="Selfie.jpg" class="btn btn-success" role="button">
+                            <a
+                                id="downloadPhoto"
+                                :href="dloadSource"
+                                download="Selfie.jpg"
+                                class="btn btn-success"
+                                role="button"
+                            >
                                 <i class="fa fa-download"></i> Download
                             </a>
-                            <button class="btn btn-info float-right" type="button" @click="newSelfie = true"> 
-                            <i class="fa fa-repeat"></i> New
+                            <button
+                                class="btn btn-info float-right"
+                                type="button"
+                                @click="newSelfie = true"
+                            >
+                                <i class="fa fa-repeat"></i> New
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>        
+        </div>
     </div>
 </template>
 <script>
@@ -55,7 +99,7 @@ import "@mediapipe/control_utils/control_utils.js";
 import "@mediapipe/drawing_utils/drawing_utils.js";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
 export default {
-    props: ['image'],
+    props: ["image"],
     data() {
         return {
             newSelfie: true,
@@ -65,61 +109,65 @@ export default {
             width: null,
             height: null,
             canvasElement: null,
-            img0 : null,
+            img0: null,
             photoTaken: null,
             photoTakenCtx: null,
-            facingMode: 'user',
+            facingMode: "user",
             camera: null,
             selfieSegmentation: null
-        }
+        };
     },
     mounted() {
-        this.canvasElement = this.$refs.output_canvas
+        this.canvasElement = this.$refs.output_canvas;
         this.ctx = this.canvasElement.getContext("2d");
         this.init();
 
         this.img0 = this.$refs.img0;
         const img1 = this.$refs.img1;
-        const onTakeContainer = this.$refs.onTakeContainer
-        const takenContainer = this.$refs.takenContainer
-
-        
+        const onTakeContainer = this.$refs.onTakeContainer;
+        const takenContainer = this.$refs.takenContainer;
     },
     computed: {
         inputVideo() {
             return this.$refs.input_video;
-        },
-    },    
-    methods:{
-        init(){
+        }
+    },
+    methods: {
+        init() {
             this.selfieSegmentation = new SelfieSegmentation({
-                locateFile: (file) => {
-                return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
-                },
+                locateFile: file => {
+                    return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+                }
             });
             this.selfieSegmentation.setOptions({
-            modelSelection: 1,
+                modelSelection: 1
             });
             this.selfieSegmentation.onResults(this.onResults);
-            this.startCamera(this.facingMode)
-                      
+            this.startCamera(this.facingMode);
         },
-        startCamera(facingMode){
+        startCamera(facingMode) {
             const camera = new Camera(this.inputVideo, {
                 onFrame: async () => {
-                    await this.selfieSegmentation.send({ image: this.inputVideo });
+                    await this.selfieSegmentation.send({
+                        image: this.inputVideo
+                    });
                 },
-                width: 1280,                
-                height: 720,
-                facingMode: facingMode              
+                width: document.body.clientWidth,
+                height: document.body.clientHeight,
+                facingMode: facingMode
             });
-            camera.start(facingMode);  
+            camera.start(facingMode);
         },
         onResults(results) {
             this.width = results.image.width;
             this.height = results.image.height;
             this.ctx.save();
-            this.ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+            this.ctx.clearRect(
+                0,
+                0,
+                this.canvasElement.width,
+                this.canvasElement.height
+            );
             this.ctx.drawImage(
                 results.segmentationMask,
                 0,
@@ -127,74 +175,85 @@ export default {
                 this.canvasElement.width,
                 this.canvasElement.height
             );
-             // Only overwrite missing pixels.
-            this.ctx.globalCompositeOperation = 'source-out';
-                
+            // Only overwrite missing pixels.
+            this.ctx.globalCompositeOperation = "source-out";
+
             this.ctx.drawImage(
-                this.img0, 0, 0, this.canvasElement.width, this.canvasElement.height);
-                
+                this.img0,
+                0,
+                0,
+                this.canvasElement.width,
+                this.canvasElement.height
+            );
+
             // Only overwrite missing pixels.
             this.ctx.globalCompositeOperation = "destination-atop";
             this.ctx.drawImage(
-                results.image, 0, 0, this.canvasElement.width, this.canvasElement.height);
+                results.image,
+                0,
+                0,
+                this.canvasElement.width,
+                this.canvasElement.height
+            );
             this.ctx.restore();
         },
-        
-        takePhoto(){
-            this.newSelfie = false
-            this.photoTaken = this.$refs.photoTaken
-            // console.log(this.photoTaken)
-            this.photoTakenCtx = this.$refs.photoTaken.getContext('2d');
-            this.photoTakenCtx.drawImage(this.canvasElement, 0, 0, 1280, 720);
-            this.dloadSource = this.photoTaken.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream")
-        },
-        handleChangeFacingMode(){
-            this.facingMode = this.facingMode == 'user' ? 'environment' : 'user'
-            this.startCamera(this.facingMode)
 
+        takePhoto() {
+            this.newSelfie = false;
+            this.photoTaken = this.$refs.photoTaken;
+            // console.log(this.photoTaken)
+            this.photoTakenCtx = this.$refs.photoTaken.getContext("2d");
+            this.photoTakenCtx.drawImage(this.canvasElement, 0, 0, 1280, 720);
+            this.dloadSource = this.photoTaken
+                .toDataURL("image/jpeg")
+                .replace("image/jpeg", "image/octet-stream");
+        },
+        handleChangeFacingMode() {
+            this.facingMode =
+                this.facingMode == "user" ? "environment" : "user";
+            this.startCamera(this.facingMode);
         },
         isMobile() {
-            if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                return true
+            if (
+                /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                    navigator.userAgent
+                )
+            ) {
+                return true;
             } else {
-                return false
+                return false;
             }
         }
     }
-    
-}
+};
 </script>
 <style scoped>
+canvas {
+    background-color: blue;
+    width: 100%;
+    height: auto;
+}
+@media screen and (max-width: 1200px) {
+    /*Tablets [601px -> 1200px]*/
     canvas {
         background-color: blue;
         width: 100%;
         height: auto;
-        
-    } 
-    @media screen and (max-width: 1200px){
-    /*Tablets [601px -> 1200px]*/
-        canvas {
-            background-color: blue;
-            width: 100%;
-            height: auto;
-            
-        }    
     }
-    @media only screen and (max-width: 425px){
-	/*Small smartphones [325px -> 425px]*/
-        canvas {
-            background-color: blue;
-            
-            max-width: 300px;
-        
-        }
+}
+@media only screen and (max-width: 425px) {
+    /*Small smartphones [325px -> 425px]*/
+    canvas {
+        background-color: blue;
 
-        video.input_video{
-            max-width: 300px;
-
-        }
+        width: 300px;
     }
-    div.card{
-        background: rgba(255, 255, 255, 0.1);
-    }    
+
+    video.input_video {
+        max-width: 300px;
+    }
+}
+div.card {
+    background: rgba(255, 255, 255, 0.1);
+}
 </style>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    @production
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    @endproduction
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Convention</title>
-    <style>
-      body {
-        /* margin: 0;
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  @production
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+  @endproduction
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Convention</title>
+  <style>
+    body {
+      /* margin: 0;
         width: 100%;
         height: 100%;
         overflow: hidden;
@@ -22,35 +23,39 @@
         background-size: cover;
         background-position: center;
         background-attachment: fixed; */
-      }
+    }
 
-      a:link, a:visited{
-        color: #bdc3c7;
-      }
+    a:link,
+    a:visited {
+      color: #bdc3c7;
+    }
 
-      .credit{
-        position: absolute;
-        text-align: center;
-        width: 100%;
-        padding: 20px 0;
-        color: #fff;
-      }
-      .input-group-prepend-120 {
-          width: 120px;
-      }
-    </style>
+    .credit {
+      position: absolute;
+      text-align: center;
+      width: 100%;
+      padding: 20px 0;
+      color: #fff;
+    }
+
+    .input-group-prepend-120 {
+      width: 120px;
+    }
+  </style>
 </head>
+
 <body>
-    
-    <div id="app">
-    </div>
-    <script src="{{ mix('js/app.js') }}?v2"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/105/three.js" integrity="sha512-wi1en5HQFr/+nc03XLj7iJohyUcclImFC3U5uOjYE+CM6FTla7scwzbuy56+Z5sIZ3sZy1KuNdjLIid4vhJMzg==" crossorigin="anonymous"></script> -->
+
+  <div id="app" ref="appRef">
+  </div>
+  <script src="{{ mix('js/app.js') }}?v2"></script>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/105/three.js" integrity="sha512-wi1en5HQFr/+nc03XLj7iJohyUcclImFC3U5uOjYE+CM6FTla7scwzbuy56+Z5sIZ3sZy1KuNdjLIid4vhJMzg==" crossorigin="anonymous"></script> -->
 
 
-    <script>
+  <script>
 
 
-    </script>
+  </script>
 </body>
+
 </html>
