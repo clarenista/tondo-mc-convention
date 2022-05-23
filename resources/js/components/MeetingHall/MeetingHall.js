ac064f6192f065c01d87f76284e8e60eba61361d
Artisan <artisan.it.services@gmail.com>
2022-05-24 02:42:11 +0800

    update

--- resources/js/components/MeetingHall/MeetingHall.js
+++ resources/js/components/MeetingHall/MeetingHall.js
@@ -1,7 +1,7 @@
 export default {
     init(vue) {
+
         let zoomBtn = vue.$el.querySelector(".open-zoom-meeting");
-        this.embedded = 'true';
         zoomBtn.style.display = "none";
         this.isAllowed(vue).then((result) => {
             if (result) {
@@ -10,19 +10,57 @@ export default {
                     this.isAllowed(vue).then((result) => {
                         vue.$store.getters.audio.pause();
                         if (result) {
-                            this.openZoomMobile(vue).then((ret)=>{
+                            this.openZoomMobile(vue).then((ret) => {
                                 if (!ret) {
                                     vue.$store.commit('updateIsAllowed', true);
                                 }
                             });
-                            // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || this.embedded == 'false') {
-                            //     this.openZoomMobile(vue);
-                            // } else {
-                            //     window.location = "/meeting-hall";
-                            // }
                         } else {
                             vue.$store.commit('updateIsAllowed', true);
-                            // zoomBtn.style.display = "none";
+                        }
+                    });
+                });
+            }
+        });
+
+        let zoomBtnRH = vue.$el.querySelector(".open-zoom-residents-hour");
+        zoomBtnRH.style.display = "none";
+        this.isAllowedRH(vue).then((result) => {
+            if (result) {
+                zoomBtnRH.style.display = "";
+                zoomBtnRH.addEventListener("click", () => {
+                    this.isAllowed(vue).then((result) => {
+                        vue.$store.getters.audio.pause();
+                        if (result) {
+                            this.openZoomRH(vue).then((ret) => {
+                                if (!ret) {
+                                    vue.$store.commit('updateIsAllowed', true);
+                                }
+                            });
+                        } else {
+                            vue.$store.commit('updateIsAllowed', true);
+                        }
+                    });
+                });
+            }
+        })
+
+        let zoomBtnBM = vue.$el.querySelector(".open-zoom-bussiness-meeting");
+        zoomBtnBM.style.display = "none";
+        this.isAllowedBM(vue).then((result) => {
+            if (result) {
+                zoomBtnBM.style.display = "";
+                zoomBtnBM.addEventListener("click", () => {
+                    this.isAllowed(vue).then((result) => {
+                        vue.$store.getters.audio.pause();
+                        if (result) {
+                            this.openZoomBM(vue).then((ret) => {
+                                if (!ret) {
+                                    vue.$store.commit('updateIsAllowed', true);
+                                }
+                            });
+                        } else {
+                            vue.$store.commit('updateIsAllowed', true);
                         }
                     });
                 });
@@ -30,11 +68,36 @@ export default {
         })
     },
     async openZoomMobile(vue) {
+        console.log('MAIN');
         let { data } = await vue.axios.get(`/api/v1/guests/zoom/join/mobile?api_token=${localStorage.getItem("access_token")}`);
-        if(data == 0){
+        if (data == 0) {
             vue.$store.commit('updateIsNotAllowedMessage', "PSP members' fellowship night going on.")
             return false;
-        }else{
+        } else {
+            window.open(data, "_blank");
+            return true;
+        }
+        return false;
+    },
+    async openZoomRH(vue) {
+        console.log('RH');
+        let { data } = await vue.axios.get(`/api/v1/guests/zoom/join/rh?api_token=${localStorage.getItem("access_token")}`);
+        if (data == 0) {
+            vue.$store.commit('updateIsNotAllowedMessage', "Residents' Hour going on.")
+            return false;
+        } else {
+            window.open(data, "_blank");
+            return true;
+        }
+        return false;
+    },
+    async openZoomBM(vue) {
+        console.log('BM');
+        let { data } = await vue.axios.get(`/api/v1/guests/zoom/join/bm?api_token=${localStorage.getItem("access_token")}`);
+        if (data == 0) {
+            vue.$store.commit('updateIsNotAllowedMessage', "Bussiness Meeting going on.")
+            return false;
+        } else {
             window.open(data, "_blank");
             return true;
         }
@@ -43,27 +106,43 @@ export default {
     async isAllowed(vue) {
         let userType = vue.$store.getters.user.classification;
         let { data } = await vue.axios.get(`/api/v1/program?api_token=${localStorage.getItem("access_token")}`);
-        // console.log(data);
         let program = data;
-        this.embedded = program.embedded;
-        if(Date.now() < program.start_at_){
-            vue.$store.commit('updateIsNotAllowedMessage', 'The Meeting has not started yet');
-            return false;
-        }
         if (program.enabled == 1) {
             return true;
-            // if (program.type == "all")
-            //     return true;
-            // if (program.type == "private" && (userType == "Diplomate" || userType == "Fellow"))
-            //     return true;
-        }else{
-            vue.$store.commit('updateIsNotAllowedMessage', 'The session is not available.');
-            return false;
         }
-        vue.$store.commit('updateIsNotAllowedMessage', 'The meeting has not yet started.')
+        vue.$store.commit('updateIsNotAllowedMessage', 'The session is not available.');
+        return false;
+    },
+
+    async isAllowedRH(vue) {
+        let userType = vue.$store.getters.user.classification;
+        let { data } = await vue.axios.get(`/api/v1/program/rh?api_token=${localStorage.getItem("access_token")}`);
+        let program = data;
+        if (program.enabled == 1) {
+            return true;
+        }
+        vue.$store.commit('updateIsNotAllowedMessage', 'The session is not available.');
+        return false;
+    },
+
+    async isAllowedBM(vue) {
+        let { data } = await vue.axios.get(`/api/v1/program/bm?api_token=${localStorage.getItem("access_token")}`);
+        let program = data;
+        if (program.enabled == 1) {
+            return true;
+        }
+        vue.$store.commit('updateIsNotAllowedMessage', 'The session is not available.');
         return false;
     },
     getZoomType(vue) {
         return vue.axios.get(`/api/v1/program?api_token=${localStorage.getItem("access_token")}`);
     },
 };
+
+
+
+// let userType = vue.$store.getters.user.classification;
+// if (Date.now() < program.start_at_) {
+//     vue.$store.commit('updateIsNotAllowedMessage', 'The Meeting has not started yet');
+//     return false;
+// }
--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -37,10 +37,14 @@ Route::post('booths/{booth_id}/wheel', [BoothController::class, 'wheelSubmit']);
 
 Route::get('/guests/zoom/join/config', [GuestController::class, 'zoomJoin']);
 Route::get('/guests/zoom/join/mobile', [GuestController::class, 'zoomJoinMobile']);
+Route::get('/guests/zoom/join/rh', [GuestController::class, 'zoomJoinRH']);
+Route::get('/guests/zoom/join/bm', [GuestController::class, 'zoomJoinBM']);
 Route::get('/guests/booths/tracker', [GuestController::class, 'boothTracker']);
 Route::post('/guests/event/push', [GuestEventController::class, 'push']);
 
 Route::get('/program', [ProgramController::class, 'get']);
+Route::get('/program/rh', [ProgramController::class, 'rh']);
+Route::get('/program/bm', [ProgramController::class, 'bm']);
 Route::get('/event', [EventController::class, 'get']);
 
 Route::get('/chat/rooms', [ChatController::class, 'rooms']);
