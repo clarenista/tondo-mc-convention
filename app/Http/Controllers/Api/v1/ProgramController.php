b7505fd6b09e7309c48b50b5730f95631746b443
Artisan <artisan.it.services@gmail.com>
2022-05-25 01:17:05 +0800

    updat

--- app/Http/Controllers/Api/v1/ProgramController.php
+++ app/Http/Controllers/Api/v1/ProgramController.php
@@ -13,6 +13,11 @@ class ProgramController extends Controller
     {
 
         $event = Program::select('start_at', 'video_url', 'type', DB::raw('`group` as embedded'), 'enabled', 'title')->whereEnabled(1)->where('id','<',5)->first();
+
+        if(!$event){
+            $event = Program::select('start_at', 'video_url', 'type', DB::raw('`group` as embedded'), 'enabled', 'title')->where(1)->first();
+
+        }
         // $event->start_at_ = strtotime($event->start_at) * 1000;
 
         return $event;
