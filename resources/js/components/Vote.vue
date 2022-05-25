72a3bbecefb4a9bd9574c57b310d315882ba5177
Lester Lou Reyes <clarenista@gmail.com>
2022-05-25 11:55:36 +0800

    update

--- resources/js/components/Vote.vue
+++ resources/js/components/Vote.vue
@@ -1,28 +1,42 @@
-<template >
-  <div class="embed-responsive embed-responsive-16by9 background full">
-    <iframe class="embed-responsive-item" :src="'https://vote.psp.com.ph/cast-vote?username='+$store.getters.user.email" allowfullscreen></iframe>
-    <img src="/images/icons/sponsor-back-btn.png " @click="handleBackToLobby" class="btn btn-sm" alt="" srcset="" style="position: fixed; top: 0; left: 0; margin:1em; z-index: 10;" width="100"> 
-  </div>
+<template>
+    <div class="embed-responsive embed-responsive-16by9 background full">
+        <iframe
+            class="embed-responsive-item"
+            :src="
+                'https://vote.psp.com.ph/cast-vote?username=' +
+                    $store.getters.user.email_address
+            "
+            allowfullscreen
+        ></iframe>
+        <img
+            src="/images/icons/sponsor-back-btn.png "
+            @click="handleBackToLobby"
+            class="btn btn-sm"
+            alt=""
+            srcset=""
+            style="position: fixed; top: 0; left: 0; margin:1em; z-index: 10;"
+            width="100"
+        />
+    </div>
 </template>
 <script>
 export default {
-  methods:{
-    handleBackToLobby(){
-            this.$router.push({ name: 'home' })
-        },
-  }
-    
-}
+    methods: {
+        handleBackToLobby() {
+            this.$router.push({ name: "home" });
+        }
+    }
+};
 </script>
 <style scoped>
-  div.background {
+div.background {
     top: 0;
     left: 0;
     position: fixed;
-  }
+}
 
-  div.full{
+div.full {
     width: 100%;
     height: 100%;
-  }      
+}
 </style>
