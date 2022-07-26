0945e8fdf680c3a3ee614e6aac4a85ef21e9979e
MJ <contact.mjpanaleon@gmail.com>
2022-07-27 00:14:00 +0800

    remove visit link

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -103,15 +103,6 @@
                                     >CONTACT US</a
                                 >
                             </h1>
-
-                            <h1 class="lead text-center text-light">
-                                <a
-                                    href="https://tmc.doh.gov.ph/"
-                                    class="btn"
-                                    target="_new"
-                                    >visit: tmc.doh.gov.ph</a
-                                >
-                            </h1>
                         </div>
                     </div>
 
