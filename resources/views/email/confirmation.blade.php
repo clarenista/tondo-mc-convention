d01b12fb1c88b765b70aa632abdd2c2ec4621ff4
undefined <email@email.com>
2022-08-02 18:56:38 +0800

    update

--- resources/views/email/confirmation.blade.php
+++ resources/views/email/confirmation.blade.php
@@ -1,8 +1,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 </head>
+
 <body>
-    <img src="https://tondomcobgyn2022.com/email-confirmation.png" style="max-width: 100%">
+    <div style="width: 100%">
+        <center>
+            <img src="https://tondomcobgyn2022.com/email-confirmation.png" style="max-width: 100%">
+        </center>
+    </div>
 </body>
+
 </html>
