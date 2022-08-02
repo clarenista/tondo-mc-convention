8df9becf165b74a95a6d3863b8e7243acec98933
undefined <email@email.com>
2022-08-02 18:38:22 +0800

    update

--- resources/views/email/confirmation.blade.php
+++ resources/views/email/confirmation.blade.php
@@ -3,6 +3,6 @@
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 </head>
 <body>
-    <img src="https://tondomcobgyn2022.com/images/email-confirmation.jpg">
+    <img src="https://tondomcobgyn2022.com/email-confirmation.jpg">
 </body>
 </html>
