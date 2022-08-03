7f5900b022f8fa8961eefc9d7d473af36f802930
undefined <email@email.com>
2022-08-04 04:16:48 +0800

    update

--- resources/views/email/reminder-1-week.blade.php
+++ resources/views/email/reminder-1-week.blade.php
@@ -9,15 +9,16 @@
         <center>
             <div style="max-width: 1024px;">
                 <table
-                    style="width: 100%; height: 100%;background: url('https://tondomcobgyn2022.com/email-reminder-1-week.png');background-repeat: no-repeat;max-height: 598px;">
+                    style="width: 1024px; height: 598px;background: url('https://tondomcobgyn2022.com/email-reminder-1-week.png');background-repeat: no-repeat;max-height: 598px;">
                     <tr>
-                        <td style="height: 75%; ">
+                        <td style="height: 75%; color:#ffffff00;">
+                            This is a reminder that "Tondo Medical Center's Post Graduate Course" will begin in 1 week;
                         </td>
                     </tr>
                     <tr>
                         <td style="height: 4%;text-align: center">
                             <a target="_blank" href="https://tondomcobgyn2022.com/login"
-                                style="width:30%;height: 5%;color:#ffffff00;">WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                                style="width:30%;height: 5%;color:#ffffff00;">https://tondomcobgyn2022.com/
                             </a>
                         </td>
                     </tr>
@@ -28,14 +29,14 @@
                     <tr>
                         <td style="height: 4%;text-align: center">
                             <a target="_blank" href="https://tmc.doh.gov.ph"
-                                style="width:20%;height: 4%;color:#ffffff00;">WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                                style="width:20%;height: 4%;color:#ffffff00;">https://tmc.doh.gov.ph
                             </a>
                         </td>
                     </tr>
                     <tr>
                         <td style="height: 4%;text-align: center">
                             <a target="_blank" href="https://www.facebook.com/tondomed"
-                                style="width:40%;height: 4%;color:#ffffff00;">WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                                style="width:40%;height: 4%;color:#ffffff00;">https://www.facebook.com/tondomed
                             </a>
                         </td>
                     </tr>
