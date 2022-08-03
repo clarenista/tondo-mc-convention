d0e45478a5de1bdfc02cd70dd61c793acb8eeaf7
undefined <email@email.com>
2022-08-04 03:40:02 +0800

    update

--- resources/views/email/reminder-1-week.blade.php
+++ resources/views/email/reminder-1-week.blade.php
@@ -7,7 +7,18 @@
 <body>
     <div style="width: 100%">
         <center>
-            <img src="https://tondomcobgyn2022.com/email-reminder-1-week.png" style="max-width: 100%">
+            <div style="max-width: 1024px;position: relative;">
+                <a target="_blank" href="https://tondomcobgyn2022.com/login"
+                    style="background-color: #ff000033; width:30%;height: 5%;top:74%;left:35%;position: absolute;">
+                </a>
+                <a target="_blank" href="https://tmc.doh.gov.ph"
+                    style="background-color: #ff000033; width:20%;height: 4%;top:86%;left:40%;position: absolute;">
+                </a>
+                <a target="_blank" href="https://www.facebook.com/tondomed"
+                    style="background-color: #ff000033; width:26%;height: 4%;top:90%;left:37%;position: absolute;">
+                </a>
+                <img src="https://tondomcobgyn2022.com/email-reminder-1-week.png" style="max-width: 1024px;width: 100%">
+            </div>
         </center>
     </div>
 </body>
