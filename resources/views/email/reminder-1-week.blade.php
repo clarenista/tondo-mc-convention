5b34b91d1f2fb03724d622f00cf8dc9104f2cbad
undefined <email@email.com>
2022-08-04 04:05:01 +0800

    update

--- resources/views/email/reminder-1-week.blade.php
+++ resources/views/email/reminder-1-week.blade.php
@@ -5,19 +5,46 @@
 </head>
 
 <body>
-    <div style="width: 100%">
+    <div style="width: 100%; ">
         <center>
-            <div style="max-width: 1024px;position: relative;">
-                <a target="_blank" href="https://tondomcobgyn2022.com/login"
-                    style="background-color: #ff000033; width:30%;height: 5%;top:74%;left:35%;position: absolute;">
-                </a>
-                <a target="_blank" href="https://tmc.doh.gov.ph"
-                    style="background-color: #ff000033; width:20%;height: 4%;top:86%;left:40%;position: absolute;">
-                </a>
-                <a target="_blank" href="https://www.facebook.com/tondomed"
-                    style="background-color: #ff000033; width:26%;height: 4%;top:90%;left:37%;position: absolute;">
-                </a>
-                <img src="https://tondomcobgyn2022.com/email-reminder-1-week.png" style="max-width: 1024px;width: 100%">
+            <div style="max-width: 1024px;">
+                <table
+                    style="width: 100%; height: 100%;background: url('https://tondomcobgyn2022.com/email-reminder-1-week.png');background-repeat: no-repeat;max-height: 598px;">
+                    <tr>
+                        <td style="height: 75%; ">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height: 4%;text-align: center">
+                            <a target="_blank" href="https://tondomcobgyn2022.com/login"
+                                style="width:30%;height: 5%;color:#ffffff00;">WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height: 8%; ">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height: 4%;text-align: center">
+                            <a target="_blank" href="https://tmc.doh.gov.ph"
+                                style="width:20%;height: 4%;color:#ffffff00;">WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height: 4%;text-align: center">
+                            <a target="_blank" href="https://www.facebook.com/tondomed"
+                                style="width:40%;height: 4%;color:#ffffff00;">WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height: 6%; ">
+                        </td>
+                    </tr>
+                </table>
+                {{-- <img src="https://tondomcobgyn2022.com/email-reminder-1-week.png" style="max-width: 1024px;width: 100%"> --}}
             </div>
         </center>
     </div>
