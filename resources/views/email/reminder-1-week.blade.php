79e09e32ac73909100a899c6c458906dad07a000
undefined <email@email.com>
2022-08-04 05:25:20 +0800

    update

--- resources/views/email/reminder-1-week.blade.php
+++ resources/views/email/reminder-1-week.blade.php
@@ -2,53 +2,65 @@
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Public Sans', sans-serif;
+        }
+    </style>
 </head>
 
 <body style="width: 100%">
     <center>
-        <table border="0" width="1024px" height="598px">
+        <table border="0" width="1024px" style="
+        border-collapse: collapse;
+    ">
             <thead>
                 <th style="width: 1024px">
                 </th>
             </thead>
             <tbody>
                 <tr style="width: 1024px">
-                    <td style="height: 75%; color:#ffffff00;">
-                        <img src="https://tondomcobgyn2022.com/email-reminder-header.png"
-                            style="width: 100%;">
+                    <td>
+                        <img src="https://tondomcobgyn2022.com/email-reminder-header.png" style="width: 100%;">
                     </td>
                 </tr>
                 <tr style="width: 1024px">
-                    <td style="height: 4%;text-align: center">
-                        <a target="_blank" href="https://tondomcobgyn2022.com/login"
-                            style="width:30%;height: 5%;color:#ffffff00;">https://tondomcobgyn2022.com/
-                        </a>
+                    <td
+                        style="height: 4%;text-align: center;background: #d4e3d9;color: #17652c;font-size: 16px;font-weight: bold;padding: 2rem;">
+                        THIS IS A REMINDER THAT "TONDO MEDICAL CENTER'S POSTGRADUATE COURSE" WILL BEGIN IN 1 WEEK ON:
+                        <br>
+                        <br>
+                        DATE TIME: AUG 11, 2022 4:00 PM SINGAPORE
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        THIS IS ALSO TO ANNOUNCE THAT TODAY IS THE FIRST DAY THAT YOU CAN ACCESS THE VIRTUAL BOOTHS.
+                        <br>
+                        <br>
+                        LINK: <a href="https://tondomcobgyn2022.com/login">https://tondomcobgyn2022.com</a>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        YOU MAY ALSO VISIT:
+                        <br>
+                        <br><a href="http://tmc.doh.gov.ph/">http://tmc.doh.gov.ph/</a>
+                        <br>
+                        <br><a href="https://www.facebook.com/tondomed">https://www.facebook.com/tondomed</a>
                     </td>
                 </tr>
                 <tr style="width: 1024px">
-                    <td style="height: 8%; ">
-                    </td>
-                </tr>
-                <tr style="width: 1024px">
-                    <td style="height: 4%;text-align: center">
-                        <a target="_blank" href="https://tmc.doh.gov.ph"
-                            style="width:20%;height: 4%;color:#ffffff00;">https://tmc.doh.gov.ph
-                        </a>
-                    </td>
-                </tr>
-                <tr style="width: 1024px">
-                    <td style="height: 4%;text-align: center">
-                        <a target="_blank" href="https://www.facebook.com/tondomed"
-                            style="width:40%;height: 4%;color:#ffffff00;">https://www.facebook.com/tondomed
-                        </a>
-                    </td>
-                </tr>
-                <tr style="width: 1024px">
-                    <td style="height: 6%; ">
+                    <td>
+                        <img src="https://tondomcobgyn2022.com/email-reminder-footer.png" style="width: 100%;">
                     </td>
                 </tr>
             </tbody>
